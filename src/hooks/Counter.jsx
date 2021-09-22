e0e377c93b8840359d02e76aa77c9bb0083513e7
Range Quest <range.quest@gmail.com>
2021-09-22 20:21:23 +0530

    useEffect hook implementations.

--- src/hooks/Counter.jsx
+++ src/hooks/Counter.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Counter(props) {
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} times`
+    return () => {
+      console.log('Clean up')
+    }
+  })
+
   return (
     <React.Fragment>
       <input type="text" name="username" id="username" onChange={e => setName(e.target.value)} />
