d5396f6ddb095920e281fe208d7da4a5434efd90
Range Quest <range.quest@gmail.com>
2021-09-22 19:30:29 +0530

    Implemented the higher order component.

---
+++ src/hoc/withTooltip.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function withTooltip(Component) {
+  return class withTooltip extends React.Component {
+    state = { showTooltip: false }
+
+    mouseOver = () => this.setState({ showTooltip: true })
+    mouseOut = () => this.setState({ showTooltip: false })
+
+    render() {
+      return (
+        <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+          <Component {...this.props} showTooltip={this.state.showTooltip} />
+        </div>
+      )
+    }
+  }
+}
+
+export default withTooltip
